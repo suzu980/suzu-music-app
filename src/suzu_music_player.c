359571cf1893ecd36eeab0b0f1343e2f3bd039bb
suzu <suzuXIV@gmail.com>
2023-10-17 19:32:01 +0800

    Color test

--- src/suzu_music_player.c
+++ src/suzu_music_player.c
@@ -9,14 +9,16 @@ int main(void) {
   const int screenWidth = 800;
   const int screenHeight = 450;
 
-  InitWindow(screenWidth, screenHeight, "Suzu Music Player");
-  const char *text = "Hello World";
+  InitWindow(screenWidth, screenHeight, "Music Player and Metadata Editor");
+  const char *text = "This app will be made with Catppuccin theme";
 
   SetTargetFPS(60); // Set our game to run at 60 frames-per-second
   int textSize = MeasureText(text, 20);
   int xpos =
       screenWidth / 2 - textSize / 2; // MeasureText(text, 20);
   //--------------------------------------------------------------------------------------
+	Color baseColor = (Color){239, 241, 245, 255};
+	Color textColor = (Color){76, 79, 105, 255};
 
   // Main game loop
   while (!WindowShouldClose()) // Detect window close button or ESC key
@@ -30,9 +32,9 @@ int main(void) {
     //----------------------------------------------------------------------------------
     BeginDrawing();
 
-    ClearBackground(RAYWHITE);
+    ClearBackground(baseColor);
 
-    DrawText(text, xpos, 200, 20, LIGHTGRAY);
+    DrawText(text, xpos, 200, 20, textColor);
 
     EndDrawing();
     //----------------------------------------------------------------------------------
