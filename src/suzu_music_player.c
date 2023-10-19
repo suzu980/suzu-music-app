ee7845aa0ceaa16b2c22eaf175b1462be5594d2e
suzu <suzuXIV@gmail.com>
2023-10-19 15:31:29 +0800

    Added way to print several lines

--- src/suzu_music_player.c
+++ src/suzu_music_player.c
@@ -1,24 +1,79 @@
 #include "raylib.h"
 #include <stdio.h>
+#include <time.h>
+
+void drawFileList(FilePathList *pFileList, Font *pNormalText, int fontSize,
+                  Color *textColor);
+
+void CustomLog(int msgType, const char *text, va_list args) {
+  char timeStr[64] = {0};
+  time_t now = time(NULL);
+  struct tm *tm_info = localtime(&now);
+
+  strftime(timeStr, sizeof(timeStr), "%Y-%m-%d %H:%M:%S", tm_info);
+  printf("[%s] ", timeStr);
+
+  switch (msgType) {
+  case LOG_INFO:
+    printf("[INFO] : ");
+    break;
+  case LOG_ERROR:
+    printf("[ERROR]: ");
+    break;
+  case LOG_WARNING:
+    printf("[WARN] : ");
+    break;
+  case LOG_DEBUG:
+    printf("[DEBUG]: ");
+    break;
+  default:
+    break;
+  }
+
+  vprintf(text, args);
+  printf("\n");
+}
 
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
+void drawFileList(FilePathList *pFileList, Font *pNormalText, int fontSize,
+                  Color *textColor) {
+  // draw file list
+  int lineSpacing = 25;
+  int size = pFileList->count;
+  Vector2 positions[size];
+  for (int idx = 0; idx < size; ++idx) {
+    positions[idx].x = 200;
+    positions[idx].y = lineSpacing * idx + 200;
+  }
+  for (int idx = 0; idx < size; ++idx) {
+    DrawTextEx(*pNormalText, GetFileNameWithoutExt(pFileList->paths[idx]), positions[idx], fontSize, 0,
+               *textColor); // DrawText(text, xpos, 200, 20, textColor);
+  }
+}
 int main(void) {
   // Initialization
   //--------------------------------------------------------------------------------------
   int screenWidth = 1280;
   int screenHeight = 720;
   const int lineBreakSpacing = 24;
+  const int titleOffset = 250;
 
   Color baseColor = (Color){239, 241, 245, 255};
   Color textColor = (Color){76, 79, 105, 255};
 
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(screenWidth, screenHeight, "Music Player and Metadata Editor");
+
+  // Getting filelists
+  FilePathList fileList =
+      LoadDirectoryFilesEx("resources/sample-music", ".flac;.mp3;.m4a", true);
+
   const char *headerText = "Suzu Music Player";
-  const char *normalText =
-      "This app will be made with Cappuccin theme. https://github.com/suzu980";
+  const char *normalText = "Just a normal music player written in C and Raylib library" ;
+  // const char *normalText =
+  //"This app will be made with Cappuccin theme. https://github.com/suzu980";
 
   SetTargetFPS(60); // Set our game to run at 60 frames-per-second
   int Heading1Size = 48.0f;
@@ -48,12 +103,12 @@ int main(void) {
     screenHeight = GetScreenHeight();
     Vector2 HeadingPosition = {
         screenWidth / 2 - HeadingVec2.x / 2,
-        screenHeight / 2 - HeadingVec2.y / 2 -
-            lineBreakSpacing}; // TODO CHANGE THE WAY TEXT IS CENTERED
+        screenHeight / 2 - HeadingVec2.y / 2 - lineBreakSpacing -
+            titleOffset}; // TODO CHANGE THE WAY TEXT IS CENTERED
     Vector2 TextPosition = {
         screenWidth / 2 - NormalTextVec2.x / 2,
-        screenHeight / 2 - NormalTextVec2.y / 2 +
-            lineBreakSpacing}; // TODO CHANGE THE WAY TEXT IS CENTERED
+        screenHeight / 2 - NormalTextVec2.y / 2 + lineBreakSpacing -
+            titleOffset}; // TODO CHANGE THE WAY TEXT IS CENTERED
     // Draw
     //----------------------------------------------------------------------------------
     BeginDrawing();
@@ -64,6 +119,7 @@ int main(void) {
                textColor); // DrawText(text, xpos, 200, 20, textColor);
     DrawTextEx(NormalText, normalText, TextPosition, mainSize, 0,
                textColor); // DrawText(text, xpos, 200, 20, textColor);
+		drawFileList(&fileList, &NormalText, mainSize, &textColor);
 
     EndDrawing();
     //----------------------------------------------------------------------------------
@@ -72,6 +128,7 @@ int main(void) {
   // De-Initialization
   UnloadFont(Heading1);
   UnloadFont(NormalText);
+  UnloadDirectoryFiles(fileList);
   //--------------------------------------------------------------------------------------
   CloseWindow(); // Close window and OpenGL context
   //--------------------------------------------------------------------------------------
