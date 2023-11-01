74940b53641bc283e7a2462048c4e08d9ffbe0db
suzu <suzuXIV@gmail.com>
2023-11-01 12:12:32 +0800

    Bug fixes

--- src/suzu_music_player.c
+++ src/suzu_music_player.c
@@ -10,8 +10,8 @@
 
 #define ARRAY_LEN(xs) sizeof(xs) / sizeof(xs[0])
 
-#define INITIALSCREEN_WIDTH 256
-#define INITIALSCREEN_HEIGHT 256
+#define INITIALSCREEN_WIDTH 640 
+#define INITIALSCREEN_HEIGHT 360 
 
 #define MOCHABASE                                                              \
   (Color) { 30, 30, 46, 255 }
@@ -63,9 +63,7 @@ void fft(float x[], size_t n, int s, complex float *out) {
     out[0] = x[0];
     return;
   }
-  // even
   fft(x, n / 2, s * 2, out);
-  // odd
   fft(x + s, n / 2, s * 2, out + n / 2);
   for (size_t k = 0; k < n / 2; ++k) {
     float a = -2 * (pi)*k / n;
@@ -76,7 +74,7 @@ void fft(float x[], size_t n, int s, complex float *out) {
   }
 }
 
-void callbackFFT(void *bufferData, unsigned int frames) {
+void callback_audio(void *bufferData, unsigned int frames) {
   size_t buffer_capacity = N;
   if (frames <= buffer_capacity - global_frames_count) {
     memcpy(global_audio_buffer + global_frames_count, bufferData,
@@ -103,9 +101,7 @@ int main(void) {
   // Initialization
   //--------------------------------------------------------------------------------------
   pi = atan2f(1, 1) * 4;
-  float twelfth = powf(2.0, 1.0 / 12.0);
   int f = 0;
-  int nyquist;
   SetConfigFlags(FLAG_WINDOW_ALWAYS_RUN);
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(INITIALSCREEN_WIDTH, INITIALSCREEN_HEIGHT, "Music Visualizer");
@@ -116,13 +112,13 @@ int main(void) {
   initialize_frame_buffer();
   Music currentMusic = LoadMusicStream("resources/sample-music/MUSIC.mp3");
 
-  nyquist = currentMusic.stream.sampleRate / 2;
-  float log_max = log10(nyquist);
-
-  AttachAudioStreamProcessor(currentMusic.stream, callbackFFT);
+  AttachAudioStreamProcessor(currentMusic.stream, callback_audio);
   PlayMusicStream(currentMusic);
-  float interpolated_magnitudes[N / 2];
-  RenderTexture2D renderTex = LoadRenderTexture(256, 1);
+  RenderTexture2D renderTex = LoadRenderTexture(256, 2);
+  float smooth[N / 2];
+  for (size_t i = 0; i < N / 2; ++i) {
+    smooth[i] = 0.0f;
+  }
 
   //--------------------------------------------------------------------------------------
 
@@ -136,11 +132,15 @@ int main(void) {
     UpdateMusicStream(currentMusic);
 
     float spectrum[N];
+    float spectrum_t[N];
     for (size_t k = 0; k < N; ++k) {
       if (k > global_frames_count) {
         spectrum[k] = 0.0;
+        spectrum_t[k] = 0.0;
       } else {
         spectrum[k] =
+            (global_audio_buffer[k].left + global_audio_buffer[k].right) * 0.5;
+        spectrum_t[k] =
             (global_audio_buffer[k].left + global_audio_buffer[k].right) * 0.5;
       }
       spectrum[k] = spectrum[k] * hannCoeffs[k];
@@ -151,97 +151,72 @@ int main(void) {
     float peak = 0.0f;
     fft(spectrum, N, 1, fft_out);
     float magnitudes[N / 2];
-    size_t M = 0;
-    //// Fixed Bin Sizes
-    // for (float k = 1.0f; k < N / 2; k *= twelfth) {
-    //   float k_next = ceil(k * twelfth);
-    //   float peak_amp = 0.0f;
-    //   float low_amp = 0.0f;
-    //   for (size_t f = (size_t)k; f < N / 2 && f < (size_t)k_next; ++f) {
-    //     float r = crealf(fft_out[f]);
-    //     float i = cimag(fft_out[f]);
-    //     float x = 20 * log10f(powf(r, 2) + powf(i, 2));
-    //     if (x > peak_amp)
-    //       peak_amp = x;
-    //   }
-    //   if (peak < peak_amp)
-    //     peak = peak_amp;
-    //   magnitudes[M++] = peak_amp;
-    // }
-    //// ==================
+
     /// LOG SCALE
     for (size_t k = 0; k < (N / 2); ++k) {
       float r = crealf(fft_out[k]);
       float i = cimag(fft_out[k]);
-      magnitudes[k] = logf(powf(r, 2) + powf(i, 2));
+      magnitudes[k] = log10f(powf(r, 2) + powf(i, 2));
+      if (isnan(magnitudes[k]) || isinf(magnitudes[k])) {
+				magnitudes[k] = 0;
+      }
       if (peak < magnitudes[k])
         peak = magnitudes[k];
+			smooth[k] += (magnitudes[k] - smooth[k])*10.0*deltaTime;
     }
-
-    // Generate blank image
-    // Image glsl_image = GenImageColor(256, 256, BLACK);
-    // int img_cells[256] = {0};
-    // for (size_t k = 0; k < (N / 2); ++k) {
-    //  int cell_width, new_color;
-    //  int xpos = k * w / M;
-    //  new_color = 255 * (magnitudes[k] / peak);
-    //  Color c = {0, 0, new_color, 255};
-    //  int x_log = (log10f(k) / log10f(N / 2)) * 256;
-    //  cell_width = 1;
-    //  if (new_color > img_cells[x_log]) {
-    //    //ImageDrawPixel(&glsl_image, x_log, 128, c);
-    //    img_cells[x_log] = new_color;
-    //  }
-    //}
-    // Texture2D glsl_tex = LoadTextureFromImage(glsl_image);
 
     BeginTextureMode(renderTex);
     ClearBackground(BLACK);
     for (size_t k = 0; k < (N / 2); ++k) {
-      int cell_width, bar_h, new_color;
-      int xpos = k * w / M;
-      bar_h = h * (magnitudes[k] / peak);
-      new_color = 255 * (magnitudes[k] / peak);
+      int cell_width, new_color;
+      new_color = 255 * (smooth[k] / peak);
+			if (new_color > 255){
+				new_color = 255;
+			}
+			if (new_color < 0){
+				new_color = 0;
+			}
       Color c = {new_color, 0, 0, 255};
       int x_log = (log10f(k) / log10f(N / 2)) * 256;
       int x_log_next = (log10f(k + 1) / log10f(N / 2)) * 256;
+			h = 128 * (smooth[k]/peak);
       cell_width = x_log_next - x_log;
-      // DrawRectangle(xpos, h - bar_h - 50, w / M - 2, bar_h, MOCHABLUE); //
-      // LOGARITMIC SCALE BUT SAME BIN SIZE
-      DrawRectangle(x_log, 0, cell_width, 1,
-                    c); // LOGARITHMIC SCALE
+      DrawRectangle(x_log, 0, cell_width, 1, c);
+			//For Debug
+      //DrawRectangle(x_log, 127-h, cell_width, h, c);
+    }
+    for (size_t k = 0; k < (N); ++k) {
+      int new_color;
+      new_color = 255 * spectrum_t[k];
+			if (new_color > 255){
+				new_color = 255;
+			}
+			if (new_color < 0){
+				new_color = 0;
+			}
+      Color c = {0, new_color, 0, 255};
+      int xy = ((float)k / (float)N) * 256;
+      DrawRectangle(xy, 1, 1, 1, c);
+			//For Debug
+      //DrawRectangle(xy, 127, 1, 128, c);
     }
     EndTextureMode();
     BeginDrawing();
     ClearBackground(MOCHABASE);
-    // DrawTextureRec(renderTex.texture,
-    //                (Rectangle){0, 0, (float)renderTex.texture.width,
-    //                            (float)-renderTex.texture.height},
-    //                (Vector2){0, 0}, WHITE);
-    // for (size_t k = 0; k < (N / 2); ++k) {
-    //   int cell_width, bar_h, new_color;
-    //   int xpos = k * w / M;
-    //   bar_h = h * (magnitudes[k] / peak);
-    //   new_color = 255 * (magnitudes[k] / peak);
-    //   Color c = {new_color, 0, 0, 255};
-    //   int x_log = (log10f(k) / log10f(N / 2)) * w;
-    //   int x_log_next = (log10f(k + 1) / log10f(N / 2)) * w;
-    //   cell_width = x_log_next - x_log;
-    //   // DrawRectangle(xpos, h - bar_h - 50, w / M - 2, bar_h, MOCHABLUE); //
-    //   // LOGARITMIC SCALE BUT SAME BIN SIZE
-    //   DrawRectangle(x_log, 0, cell_width, h,
-    //                 c); // LOGARITHMIC SCALE
-    // }
-    //  DrawFPS(50, 50);
-    EndDrawing();
-    // UnloadImage(glsl_image);
-    // UnloadTexture(glsl_tex);
+		//To Check Frame Buffer Tex
+    //DrawTextureRec(renderTex.texture,
+    //               (Rectangle){0, 0, (float)renderTex.texture.width,
+    //                           (float)-renderTex.texture.height},
+    //               (Vector2){0, 0}, WHITE);
 
+    DrawFPS(50, 50);
+    EndDrawing();
     //----------------------------------------------------------------------------------
   }
 
   UnloadMusicStream(currentMusic);
-  DetachAudioStreamProcessor(currentMusic.stream, callbackFFT);
+  DetachAudioStreamProcessor(currentMusic.stream, callback_audio);
+	UnloadRenderTexture(renderTex);
 
   CloseAudioDevice();
   freeVariables();
